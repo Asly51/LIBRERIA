a87efa2698b4048b1e85b250e85bcbad72a39da4
Asly51 <161175185+Asly51@users.noreply.github.com>
2024-10-25 22:44:46 -0400

    Add files via upload

---
+++ Libreria Virtual.A/Libreria_Asly_Moya/Libreria_Asly_Moya/Program.vb
@@ -0,0 +1,51 @@
+Imports System
+
+Module LibraryApp
+    Dim miLibro As New LibroAs("El Alquimista", "Paulo Cohelo")
+
+    Public Property miLibro1 As LibroAs
+        Get
+            Return miLibro
+        End Get
+        Set(value As LibroAs)
+            miLibro = value
+        End Set
+    End Property
+
+    Sub Main()
+        Dim opcion As Integer
+
+        Do
+            Console.Clear()
+            Console.WriteLine("=== Librería Virtual ===")
+            Console.WriteLine("1. Prestar libro")
+            Console.WriteLine("2. Devolver libro")
+            Console.WriteLine("3. Salir")
+            Console.Write("Seleccione una opción: ")
+
+            opcion = Convert.ToInt32(Console.ReadLine())
+
+            Select Case opcion
+                Case 1
+                    PrestarLibro()
+                Case 2
+                    DevolverLibro()
+                Case 3
+                    Console.WriteLine("Saliendo del sistema...")
+                Case Else
+                    Console.WriteLine("Opción no válida.")
+            End Select
+
+            Console.WriteLine("Presione una tecla para continuar...")
+            Console.ReadKey()
+        Loop While opcion <> 3
+    End Sub
+
+    Sub PrestarLibro()
+        Console.WriteLine(miLibro.Prestar())
+    End Sub
+
+    Sub DevolverLibro()
+        Console.WriteLine(miLibro.Devolver())
+    End Sub
+End Module
